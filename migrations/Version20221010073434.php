efdb6a51654c44cd9209ad53fa1437070560804f
Louis-2204 <louis.lamouche2204@gmail.com>
2022-10-10 11:31:08 +0200

    Add registration and deleted some old migrations(no idea how they ended here) that blocked new ones

--- migrations/Version20221010073434.php
+++ migrations/Version20221010073434.php
@@ -10,22 +10,22 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20221008214318 extends AbstractMigration
+final class Version20221010073434 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return '';
+        return 'Add is_verified field to users table';
     }
 
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE users (id INT AUTO_INCREMENT NOT NULL, first_name VARCHAR(255) NOT NULL, last_name VARCHAR(255) NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_1483A5E9E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE users ADD is_verified TINYINT(1) NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP TABLE users');
+        $this->addSql('ALTER TABLE users DROP is_verified');
     }
 }
