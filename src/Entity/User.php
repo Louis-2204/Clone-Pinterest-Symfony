591a86ffca1e80238eead683760e22b12984df5d
Louis-2204 <louis.lamouche2204@gmail.com>
2022-10-11 14:49:51 +0200

    Add user profile page

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -222,6 +222,11 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         return $this->getFirstName() . ' ' . $this->getLastName();
     }
 
+    public function gravatar(?int $size = 100)
+    {
+        return sprintf('https://www.gravatar.com/avatar/%s/?s=%d', md5(strtolower(trim($this->getEmail()))),  $size);
+    }
+
     public function isVerified(): bool
     {
         return $this->isVerified;
